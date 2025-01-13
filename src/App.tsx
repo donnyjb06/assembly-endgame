3e92f42ddc6db532696094b250f94daeb9c0476a
DonovanJabbar <djabbar51@gmail.com>
2025-01-12 18:53:00 -0700

    feat: Implement confetti when game is over
    - import react-confetti npm package
    - display green confetti when game is won
    - display red confetti when game is lost
    - only displays confetti when game is over

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,13 @@ import Button from "@components/Button/Button";
 import React from "react";
 import { languages } from "@utils/languages";
 import { getRandomWord } from "@utils/words";
+import Confetti from "react-confetti";
+import ReactConfetti from "react-confetti";
 
 function App() {
 	// State variables
-	const [currentWord, setcurrentWord] = React.useState<string>(
-		() => getRandomWord()
+	const [currentWord, setcurrentWord] = React.useState<string>(() =>
+		getRandomWord()
 	);
 	const [guessedLetters, setGuessedLetters] = React.useState<Set<string>>(
 		new Set()
@@ -47,10 +49,26 @@ function App() {
 	};
 
 	const lastGuessedLetter = Array.from(guessedLetters).pop();
-	const isLastGuessedLetterCorrect = lastGuessedLetter && currentWord.includes(lastGuessedLetter);
+	const isLastGuessedLetterCorrect =
+		lastGuessedLetter && currentWord.includes(lastGuessedLetter);
 
 	return (
 		<>
+			{isGameOver && isGameWon && (
+				<Confetti
+					recycle={false}
+					numberOfPieces={1000}
+					colors={["#32CD32", "#39FF14", "#7FFF00", "#00FF7F", "#7CFC00"]}
+				/>
+			)}
+			{isGameOver && isGameLost && (
+				<Confetti
+					numberOfPieces={500}
+					colors={["#8B0000", "#800000", "#B22222", "#A52A2A", "#9D5C5C"]}
+					recycle={false}
+					gravity={0.5}
+				/>
+			)}
 			<Header />
 			<StatusSection
 				isGameWon={isGameWon}
@@ -67,20 +85,22 @@ function App() {
 				isGameOver={isGameOver}
 			/>
 
-			<section
-				className="screen-reader-only"
-				aria-live="polite"
-				role="status"
-			>
+			<section className="screen-reader-only" aria-live="polite" role="status">
 				<p>
 					{isLastGuessedLetterCorrect
-					? `Correct! The letter ${lastGuessedLetter} is in the word.`
-					: `Incorrect! The letter ${lastGuessedLetter} is not in the word.`}
+						? `Correct! The letter ${lastGuessedLetter} is in the word.`
+						: `Incorrect! The letter ${lastGuessedLetter} is not in the word.`}
 					You have ${amountOfGuessesLeft} guesses left.
 				</p>
-				<p>Current word: {currentWord.split("").map(letter =>
-					guessedLetters.has(letter) ? letter + "." : "blank."
-				).join(" ")}</p>
+				<p>
+					Current word:{" "}
+					{currentWord
+						.split("")
+						.map((letter) =>
+							guessedLetters.has(letter) ? letter + "." : "blank."
+						)
+						.join(" ")}
+				</p>
 			</section>
 
 			<Keyboard
